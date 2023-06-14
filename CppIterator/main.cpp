ae96bf4b68c2638d4475632505600b69ca61105f
matthcol <matthias.colin@gmail.com>
2023-06-14 10:57:51 +0200

    some c++/java iterator

---
+++ CppIterator/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <list>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+template <class Iterator>
+void printlnIterable(Iterator first, Iterator last, std::string bullet) {
+    while (first != last) {
+        std::cout << bullet << *first << std::endl;
+        ++first;
+    }
+}
+
+void iterateContainers() {
+    std::vector<std::string> cities { "Toulouse", "Pau", "Marseille", "Lyon", "Paris"};
+    printlnIterable(cities.cbegin(), cities.cend(), " - ");
+    printlnIterable(cities.crbegin(), cities.crend(), " # ");
+    printlnIterable(cities.cbegin()+1, cities.cend()-1, " ~ ");
+    
+    std::list<std::string> cityList(cities.cbegin(), cities.cend());
+    printlnIterable(cityList.cbegin(), cityList.cend(), " @ ");
+}
+
+
+int main (){
+    iterateContainers();
+    return EXIT_SUCCESS;
+}
---
+++ ExressionArithmetique/src/test/java/iterator/IteratorExamples.java
@@ -0,0 +1,2 @@
+package iterator;public class IteratorExmples {
+}
